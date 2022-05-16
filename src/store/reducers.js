9c1c90d332890b763da49d8e7626e7db932c087a
allan <allan.lelous@gmail.com>
2022-05-16 11:55:54 +0200

    installation redux, optimisation reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,5 @@
 import * as actions from "./actions";
-
+import { combineReducers } from "redux";
 // {
 //     todos: [],
 //     filter: ''
@@ -8,40 +8,51 @@ import * as actions from "./actions";
 //     name: '',
 //     done: true
 // }
-
-export const todoReducer = (state, action) => {
+const todos = (state, action) => {
   switch (action.type) {
     case actions.ADD_TODO: {
-      return {
-        ...state,
-        todos: [...state.todos, action.todo],
-      };
+      return [...state, action.todo];
     }
+
     case actions.DELETE_TODO: {
-      return {
-        ...state,
-        todos: state.todos.filter((t, i) => i !== action.index),
-      };
+      return state.filter((t, i) => i !== action.index);
     }
-    case actions.SET_FILTER: {
-      return {
-        ...state,
-        filter: action.filter,
-      };
-    }
+
     case actions.TOGGLE_TODO: {
-      return {
-        ...state,
-        todo: state.todos.map((t, i) => {
-          if (i === action.index) {
-            t.done = !t.done;
-          }
-          return t;
-        }),
-      };
+      return state.map((t, i) => {
+        if (i === action.index) {
+          t.done = !t.done;
+        }
+        return t;
+      });
     }
     default: {
       return state;
     }
   }
 };
+
+const filter = (state, action) => {
+  switch (action.type) {
+    case actions.SET_FILTER: {
+      return action.filter;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+// export const todosReducer = (state, action) => {
+//   return {
+//     todos: todoReducer(state.todos, action),
+//     filter: filterReducer(state.filter, action),
+//   };
+// };
+// OU
+// export const todosReducer = combineReducers ({
+//     todos: todoReducer,
+//     filter: filterReducer
+// })
+// OU
+export const todosReducer = combineReducers({ todos, filter });
